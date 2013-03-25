490c1491f1575326b01dde68348bc34df795bd82
Frédéric Gaudet <fred@linuxmint.com>
2013-03-25 13:39:52 +0100

    Update themes

--- etc/skel/.thunderbird/so8mthku.default/extensions/{e2fda1a4-762b-4020-b5ad-a41df1933103}/defaults/preferences/lightning.js
+++ etc/skel/.thunderbird/so8mthku.default/extensions/{e2fda1a4-762b-4020-b5ad-a41df1933103}/defaults/preferences/lightning.js
@@ -119,7 +119,7 @@ pref("calendar.threading.latency ", 250);
 pref("calendar.network.multirealm", false);
 
 // Set up user agent
-pref("calendar.useragent.extra", "Lightning/1.9");
+pref("calendar.useragent.extra", "Lightning/1.9.1");
 
 // Disable use of system colors in minimonth and calendar views
 pref("calendar.view.useSystemColors", false);
